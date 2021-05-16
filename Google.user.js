f20d533fe06d9b5beedbe6b36a59e1f7661ad6af
Sonny Razzano <srazzano@gmail.com>
2021-05-16 14:48:51 -0700

    Update Google.user.js

--- Google.user.js
+++ Google.user.js
@@ -1,14 +1,19 @@
 // ==UserScript==
-// @name         Google
-// @namespace    srazzano
-// @version      1.0.1
-// @description  Layout and Theme
-// @author       Sonny Razzano a.k.a. srazzano
-// @match        https://www.google.com/*
-// @icon         https://raw.githubusercontent.com/srazzano/Images/master/googleicon64.png
-// @grant        none
+// @name          Pale Moon Forum
+// @id            srazzano/Pale Moon Forum@greasespot.net
+// @version       1.0.2
+// @namespace     srazzano
+// @description   Pale Moon Forum Tweaks
+// @author        Sonny Razzano
+// @include       https://forum.palemoon.org*
+// @icon          https://raw.githubusercontent.com/srazzano/Images/master/logo.png
+// @homepageURL   https://github.com/srazzano/Pale_Moon_Forum
+// @downloadURL   https://raw.githubusercontent.com/srazzano/Pale_Moon_Forum/master/Pale_Moon_Forum.user.js
+// @support       srazzano@gmail.com
+// @grant         GM_addStyle
+// @grant         GM_getValue
+// @grant         GM_setValue
 // ==/UserScript==
-
 (function() {
 
   'use strict';

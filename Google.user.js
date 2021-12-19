606d665b38fd9a4abbf2c4378befb6c14566bfda
Sonny Razzano <srazzano@gmail.com>
2021-12-19 02:04:02 -0800

    Update Google.user.js

--- Google.user.js
+++ Google.user.js
@@ -660,12 +660,12 @@
     '}'+
     '#googImg {'+
     '  background: url('+ googleImage +') no-repeat !important;'+
-    '  margin-bottom: 20px !important;'+
+    '  margin-bottom: 16px !important;'+
     '  min-height: 140px !important;'+
     '  padding-left: 436px !important;'+
     '  pointer-events: none !important;'+
     '  position: relative !important;'+
-    '  top: 20px !important;'+
+    '  top: 12px !important;'+
     '  width: 0 !important;'+
     '}'+
     '.RNNXgb, #dateTime {'+

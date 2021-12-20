787d9ff4d6671e801a258976d6103c20f1c32892
Sonny Razzano <srazzano@gmail.com>
2021-12-20 06:07:15 -0800

    Update Google.user.js

--- Google.user.js
+++ Google.user.js
@@ -2,7 +2,7 @@
 // @name         Google Wallpaper
 // @namespace    srazzano
 // @version      1.0.1
-// @description  Layout and Theme
+// @description  Wallpaper Themes
 // @author       Sonny Razzano a.k.a. srazzano
 // @match        https://*.google.*
 // @include      https://www.google.com*
@@ -16,15 +16,15 @@
 
   'use strict';
 
-  const themerInterval = 60000,
+  const themerInterval = 30000,
+        wallpaperDefault = 1, // 1 - 13
         changeThemeText = 'Change theme hourly:',
-        defaultBackgroundImage = 1, // 1 - 13
-        //googleImage = 'https://raw.githubusercontent.com/srazzano/Images/master/googleImage.png', // GitHub site
-        googleImage = 'https://sonco.synthasite.com/resources/googleImage.png', // Yola site
-        //themerBackgroundImage = 'https://raw.githubusercontent.com/srazzano/Images/master/image', // GitHub site
-        themerBackgroundImage = 'https://sonco.synthasite.com/resources/image'; // Yola site
+        //googleLogo = 'https://raw.githubusercontent.com/srazzano/Images/master/googleImage.png', // GitHub site
+        googleLogo = 'https://sonco.synthasite.com/resources/googleImage.png', // Yola site
+        //wallpaper = 'https://raw.githubusercontent.com/srazzano/Images/master/image', // GitHub site
+        wallpaper = 'https://sonco.synthasite.com/resources/image'; // Yola site
 
-  var changeInterval;
+  var timer;
 
   function $c(type, props) {
     let node = document.createElement(type);
@@ -47,22 +47,18 @@
       else if (hour === 0) hour = 12;
       else hour = hour;
       GM_setValue('themeNumber', hour);
-      bod.style.background = "url("+ themerBackgroundImage + hour +".jpg) no-repeat center center / cover";
+      bod.style.background = 'url('+ wallpaper + hour +'.jpg) no-repeat center center / cover';
       btn.innerHTML = changeThemeText + '  On';
     } else {
-      GM_setValue('themeNumber', defaultBackgroundImage);
-      bod.style.background = "url("+ themerBackgroundImage + defaultBackgroundImage +".jpg) no-repeat center center / cover";
+      GM_setValue('themeNumber', wallpaperDefault);
+      bod.style.background = 'url('+ wallpaper + wallpaperDefault +'.jpg) no-repeat center center / cover';
       btn.innerHTML = changeThemeText + '  Off';
   } }
 
-  function onClose() {
-    clearInterval(changeInterval);
-  }
-
-  function onLoad() {
+  function initialize() {
     let signIn = $q('a.gb_3.gb_4.gb_3d.gb_3c'),
         pop = $q('#dEjpnf'),
-        li = $c('li', {role: "none"}),
+        li = $c('li', {role: 'none'}),
         btn = $c('button', {id: 'buttonThemer', className: 'EzVRq', onclick: () => themeChanger()});
     li.appendChild(btn);
     pop.appendChild(li);
@@ -70,9 +66,13 @@
     setThemer();
   }
 
+  function onClose() {
+    clearInterval(timer);
+  }
+
   function setThemer() {
-    if (GM_getValue('themeChanger')) changeInterval = setInterval(() => changeBg(), themerInterval);
-    else clearInterval(changeInterval);
+    if (GM_getValue('themeChanger')) timer = setInterval(() => changeBg(), themerInterval);
+    else clearInterval(timer);
     changeBg();
   }
 
@@ -83,14 +83,15 @@
   }
 
   if (!GM_getValue('themeChanger')) GM_setValue('themeChanger', false);
-  if (!GM_getValue('themeNumber')) GM_setValue('themeNumber', defaultBackgroundImage);
+  if (!GM_getValue('themeNumber')) GM_setValue('themeNumber', wallpaperDefault);
 
-  window.onload = onLoad();
-  window.onunload = onClose();
+  initialize();
+
+  window.addEventListener('unload', function() {onClose()});
 
   GM_addStyle(''+
     '#hplogo {'+
-    '  background: url('+ googleImage +') no-repeat !important;'+
+    '  background: url('+ googleLogo +') no-repeat !important;'+
     '  height: 140px !important;'+
     '  padding-left: 436px !important;'+
     '  width: 0 !important;'+
@@ -99,12 +100,34 @@
     '  background: transparent !important;'+
     '}'+
     '#Mses6b {'+
-    '  background: #111 !important;'+
+    '  background: #181A1B !important;'+
+    '  border: 1px solid #999 !important;'+
     '  border-radius: 4px !important;'+
+    '  color: #999 !important;'+
+    '  height: 36px !important;'+
+    '  padding: 0 10px !important;'+
+    '}'+
+    '#Mses6b:hover {'+
     '  color: #FFF !important;'+
     '}'+
     '.EzVRq {'+
+    '  color: #999 !important;'+
+    '  text-decoration: none !important;'+
+    '}'+
+    '.EzVRq:hover {'+
+    '  background-color: #111 !important;'+
     '  color: #FFF !important;'+
+    '}'+
+    '.KxwPGc.AghGtd,'+
+    '.KxwPGc.ssOUyb,'+
+    '.KxwPGc.iTjxkf > a {'+
+    '  display: none !important;'+
+    '}'+
+    '.KxwPGc.iTjxkf {'+
+    '  margin-left: 15px !important;'+
+    '  position: absolute !important;'+
+    '  top: 560px !important;'+
+    '  width: 50% !important;'+
     '}'+
   '');
 

10ab123301e9062961405f6e892e4502cadded09
Sonny Razzano <srazzano@gmail.com>
2022-02-06 00:23:01 -0800

    Update Google.user.js

--- Google.user.js
+++ Google.user.js
@@ -175,8 +175,8 @@
       case 6: return w + space + bullet + space + m + slash + d + slash + yyyy + space + bullet + space + hr12 + min + sec + space + ampm; // Sun. • 3/1/2021 • 12:34 AM
       case 7: return w + space + bullet + space + mm + slash + dd + slash + yyyy + space + bullet + space + hr12 + min + sec + space + ampm; // Sun. • 03/01/2021 • 12:34 AM
       // Delete "customFormat + 210/customFormat + 211" text below and add return options with bullet, comma, hyphen, slash, space, star characters.
-      case 8: return customFormat + 177;
-      case 9: return customFormat + 178;
+      case 8: return customFormat + 178;
+      case 9: return customFormat + 179;
   } }
 
   function dateTimeDefault() {

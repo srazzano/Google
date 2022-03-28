89780bc9c5508e3e2982d361ee6e9fc47381e1a0
Sonny Razzano <srazzano@gmail.com>
2022-03-28 00:52:26 -0700

    Update Google.user.js

--- Google.user.js
+++ Google.user.js
@@ -4,7 +4,7 @@
 // @version      1.0.1
 // @description  Layout and Theme
 // @author       Sonny Razzano a.k.a. srazzano
-// @match        https://www.google.com
+// @include      https://www.google.com*
 // @icon         https://raw.githubusercontent.com/srazzano/Images/master/googleicon64.png
 // @grant        GM_addStyle
 // @grant        GM_getValue
